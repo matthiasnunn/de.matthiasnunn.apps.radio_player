e71898760bf02f30338634a9fca81f05c87c783b
Matthias Nunn <matthiasnunn@gmx.de>
2024-07-06 00:25:03 +0200

    Add Windows support

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
+#include <just_audio_windows/just_audio_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
+  JustAudioWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("JustAudioWindowsPlugin"));
+}
